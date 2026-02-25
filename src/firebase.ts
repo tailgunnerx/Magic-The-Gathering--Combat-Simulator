a262b23241936c053b845ac2f01f7ef4bb877ff3
tailgunnerx <creedsecurity@live.com>
2026-02-25 01:17:46 -0500

    Hide Firebase API key with Xs to prevent warning emails

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
     projectId: "mtgcombat",
     appId: "1:419018442824:web:5b9aa3c7d5df94d8f1307d",
     storageBucket: "mtgcombat.firebasestorage.app",
-    apiKey: "AIzaSyBPD6EmcQz04qUmeHui18U3x2hx_IFvRn4",
+    apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     authDomain: "mtgcombat.firebaseapp.com",
     messagingSenderId: "419018442824",
     measurementId: "G-8Y7M4GZJNT",
